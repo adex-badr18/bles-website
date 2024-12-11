367300053af53e55c9db44f6d7c79cfbcb5ccbed
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-12-11 15:51:30 +0100

    feat: Design Testimonials section

---
+++ src/pages/landing/components/TestimonialCard.jsx
@@ -0,0 +1,35 @@
+import { MdOutlineFormatQuote } from "react-icons/md";
+
+const TestimonialCard = ({ image, message, name, profession }) => {
+    return (
+        <div className="flex items-start gap-6">
+            <div className="hidden md:block flex-shrink-0">
+                <img
+                    src={image}
+                    alt={name}
+                    className="w-[150px] h-[150px] rounded-full object-cover"
+                />
+            </div>
+
+            <div className="content-wrapper flex-1 space-y-4">
+                <p className="text-grey text-base leading-[28px] font-rubik">
+                    {message}
+                </p>
+
+                <div className="flex items-start gap-2">
+                    <MdOutlineFormatQuote className="text-vividRed text-[35px]" />
+                    <div className="">
+                        <h3 className="text-darkBlue text-xl font-semibold leading-[32px]">
+                            {name}
+                        </h3>
+                        <p className="text-grey font-rubik leading-[28px]">
+                            {profession}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TestimonialCard;
