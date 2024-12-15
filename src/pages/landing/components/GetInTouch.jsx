8c23e524586457bf81634ea8edbc7fba39fa41ca
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-12-15 08:50:48 +0100

    feat: Add scroll animation to GetInTouch section

--- src/pages/landing/components/GetInTouch.jsx
+++ src/pages/landing/components/GetInTouch.jsx
@@ -7,15 +7,27 @@ const GetInTouch = () => {
             <div className="container">
                 <div className="text-white space-y-5 w-full md:w-7/12">
                     <h2 className="font-poppins">
-                        <span className="font-semibold text-xl leading-[30px]">
+                        <span
+                            className="font-semibold text-xl leading-[30px]"
+                            data-aos="fade-up"
+                            data-aos-delay="200"
+                        >
                             Get In Touch
                         </span>
-                        <span className="block mt-5 text-3xl md:text-5xl leading-[40px] md:leading-[58px] font-bold">
+                        <span
+                            className="block mt-5 text-3xl md:text-5xl leading-[40px] md:leading-[58px] font-bold"
+                            data-aos="fade-up"
+                            data-aos-delay="500"
+                        >
                             Best Behavioral & Health Care Near Your City
                         </span>
                     </h2>
 
-                    <p className="font-rubik text-lg leading-[28px]">
+                    <p
+                        className="font-rubik text-lg leading-[28px]"
+                        data-aos="fade-up"
+                        data-aos-delay="1000"
+                    >
                         We've 25 years of experience in Behavioral Health
                         services.
                     </p>
@@ -28,6 +40,8 @@ const GetInTouch = () => {
                         bgColor="red"
                         to="/contact"
                         classAttrs=""
+                        data-aos="fade-right"
+                        data-aos-delay="1500"
                     />
                     <LinkButton
                         name="Specialists"
@@ -35,6 +49,8 @@ const GetInTouch = () => {
                         bgColor=""
                         to="/specialists"
                         classAttrs=""
+                        data-aos="fade-left"
+                        data-aos-delay="1500"
                     />
                 </div>
             </div>
