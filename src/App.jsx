3839fa203cbbf0029e389c9871e17300e4a82b0d
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-12-20 17:58:25 +0100

    feat: Add services route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/landing/Home";
+import Services from "./pages/services/Services";
 
 const router = createBrowserRouter([
     {
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
         element: <Layout />,
         children: [
             { index: true, element: <Home /> },
+            { path: "/services", element: <Services /> },
             { path: "*", element: <div>Error!</div> },
         ],
     },
