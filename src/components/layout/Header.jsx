2f824ddb2a6284071c8858494337ce1f588d0b6f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2025-01-30 10:47:42 +0100

    refactor: Set max width for header & navbar

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -12,53 +12,55 @@ import { MdOutlineAccessTime } from "react-icons/md";
 const Header = () => {
     return (
         <header className="font-rubik text-default">
-            <div className="bg-darkBlue flex flex-col sm:flex-row md:items-center justify-between gap-4 px-default py-[10px]">
-                <div className="flex flex-col md:flex-row gap-1 md:gap-0 md:divide-x-[3px] divide-[#909BA2]">
-                    <FlexItem
-                        icon={
-                            <MdOutlineAccessTime
-                                className="text-lightGreen"
-                                size="20"
-                            />
-                        }
-                        title="Working Hour"
-                        text="9:00am - 6:00pm"
-                        classAttrs="md:pr-3"
-                    />
-                    <FlexItem
-                        icon={
-                            <FaRegEnvelope
-                                className="text-lightGreen"
-                                size="20"
-                            />
-                        }
-                        title="Email"
-                        text="info@blesomhc.com"
-                        classAttrs="md:pl-3"
-                    />
-                </div>
-
-                <div className="hidden sm:flex flex-col md:flex-row justify-center md:justify-normal gap-1 md:gap-0 md:divide-x-[3px] divide-[#909BA2]">
-                    <div className="flex items-center flex-wrap gap-3 md:pr-3">
-                        {headerSocialData.map((social) => (
-                            <SocialLink
-                                key={social.id}
-                                icon={social.icon}
-                                to={social.to}
-                                type="header"
-                            />
-                        ))}
+            <div className="bg-darkBlue">
+                <div className="wrapper flex flex-col sm:flex-row md:items-center justify-between gap-4 px-default py-[10px]">
+                    <div className="flex flex-col md:flex-row gap-1 md:gap-0 md:divide-x-[3px] divide-[#909BA2]">
+                        <FlexItem
+                            icon={
+                                <MdOutlineAccessTime
+                                    className="text-lightGreen"
+                                    size="20"
+                                />
+                            }
+                            title="Working Hour"
+                            text="9:00am - 6:00pm"
+                            classAttrs="md:pr-3"
+                        />
+                        <FlexItem
+                            icon={
+                                <FaRegEnvelope
+                                    className="text-lightGreen"
+                                    size="20"
+                                />
+                            }
+                            title="Email"
+                            text="info@blesomhc.com"
+                            classAttrs="md:pl-3"
+                        />
                     </div>
 
-                    <p className="text-white md:pl-3">
-                        <span className="font-bold">Hotline: </span>
-                        <Link
-                            to={`tel:+19882655`}
-                            className="hover:text-lightGreen transition duration-500"
-                        >
-                            +1(410)-988-2655
-                        </Link>
-                    </p>
+                    <div className="hidden sm:flex flex-col md:flex-row justify-center md:justify-normal gap-1 md:gap-0 md:divide-x-[3px] divide-[#909BA2]">
+                        <div className="flex items-center flex-wrap gap-3 md:pr-3">
+                            {headerSocialData.map((social) => (
+                                <SocialLink
+                                    key={social.id}
+                                    icon={social.icon}
+                                    to={social.to}
+                                    type="header"
+                                />
+                            ))}
+                        </div>
+
+                        <p className="text-white md:pl-3">
+                            <span className="font-bold">Hotline: </span>
+                            <Link
+                                to={`tel:+19882655`}
+                                className="hover:text-lightGreen transition duration-500"
+                            >
+                                +1(410)-988-2655
+                            </Link>
+                        </p>
+                    </div>
                 </div>
             </div>
 
--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -67,107 +67,106 @@ const Nav = () => {
                 isSticky ? "fixed top-0 left-0 shadow w-full z-50" : ""
             } px-default py-[10px] lg:py-0`}
         >
-            <div className="flex items-center justify-between gap-4">
-                <Link to="/" className="">
-                    <Logo />
-                </Link>
+            <div className="wrapper">
+                <div className="flex items-center justify-between gap-4">
+                    <Link to="/" className="">
+                        <Logo />
+                    </Link>
 
-                <div className="hidden lg:flex items-center justify-center flex-wrap">
-                    {navMenu.map((menuItem) =>
-                        menuItem.name.toLowerCase() === "services" ? (
-                            <div
-                                key={menuItem.id}
-                                to={menuItem.to}
-                                className={`relative px-default py-7 font-rubik text-[17px] font-medium text-darkBlue transition duration-300 hover:text-vividRed ${
-                                    isServicesActive ? "text-vividRed" : ""
-                                } cursor-pointer`}
-                                onMouseEnter={() => handleMouseHover(menuItem)}
-                                onMouseLeave={() => handleMouseLeave(menuItem)}
-                            >
-                                {menuItem.name}
+                    <div className="hidden lg:flex items-center justify-center flex-wrap">
+                        {navMenu.map((menuItem) =>
+                            menuItem.name.toLowerCase() === "services" ? (
                                 <div
-                                    className={`transform ${
-                                        isServicesHovered
-                                            ? "translate-x-0"
-                                            : "-translate-x-[100vw]"
-                                    } absolute top-20 left-0 divide-y z-50 shadow-lg divide-[#e5e5e52b] bg-darkBlue text-white transition delay-100 duration-300 ease-in-out`}
+                                    key={menuItem.id}
+                                    to={menuItem.to}
+                                    className={`relative px-default py-7 font-rubik text-[17px] font-medium text-darkBlue transition duration-300 hover:text-vividRed ${
+                                        isServicesActive ? "text-vividRed" : ""
+                                    } cursor-pointer`}
+                                    onMouseEnter={() =>
+                                        handleMouseHover(menuItem)
+                                    }
+                                    onMouseLeave={() =>
+                                        handleMouseLeave(menuItem)
+                                    }
                                 >
-                                    {menuItem.subMenu?.map((menu) => (
-                                        <NavLink
-                                            key={menu.id}
-                                            to={menu.to}
-                                            className={`px-4 py-2 font-rubik text-[17px] font-medium block whitespace-nowrap ${
-                                                pathname === menu.to
-                                                    ? "bg-lightGreen text-white"
-                                                    : ""
-                                            } hover:bg-lightGreen hover:text-white`}
-                                        >
-                                            {menu.name}
-                                        </NavLink>
-                                    ))}
+                                    {menuItem.name}
+                                    <div
+                                        className={`transform ${
+                                            isServicesHovered
+                                                ? "translate-x-0"
+                                                : "-translate-x-[100vw]"
+                                        } absolute top-20 left-0 divide-y z-50 shadow-lg divide-[#e5e5e52b] bg-darkBlue text-white transition delay-100 duration-300 ease-in-out`}
+                                    >
+                                        {menuItem.subMenu?.map((menu) => (
+                                            <NavLink
+                                                key={menu.id}
+                                                to={menu.to}
+                                                className={`px-4 py-2 font-rubik text-[17px] font-medium block whitespace-nowrap ${
+                                                    pathname === menu.to
+                                                        ? "bg-lightGreen text-white"
+                                                        : ""
+                                                } hover:bg-lightGreen hover:text-white`}
+                                            >
+                                                {menu.name}
+                                            </NavLink>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                        ) : menuItem.name.toLowerCase() === "programs" ? (
-                            <div
-                                key={menuItem.id}
-                                to={menuItem.to}
-                                className={`relative px-default py-7 font-rubik text-[17px] font-medium text-darkBlue transition duration-300 hover:text-vividRed ${
-                                    isProgramsActive ? "text-vividRed" : ""
-                                } cursor-pointer`}
-                                onMouseEnter={() => handleMouseHover(menuItem)}
-                                onMouseLeave={() => handleMouseLeave(menuItem)}
-                            >
-                                {menuItem.name}
+                            ) : menuItem.name.toLowerCase() === "programs" ? (
                                 <div
-                                    className={`transform ${
-                                        isProgramsHovered
-                                            ? "translate-x-0"
-                                            : "-translate-x-[100vw]"
-                                    } absolute top-20 left-0 divide-y z-50 shadow-lg divide-[#e5e5e52b] bg-darkBlue text-white transition delay-100 duration-300 ease-in-out`}
+                                    key={menuItem.id}
+                                    to={menuItem.to}
+                                    className={`relative px-default py-7 font-rubik text-[17px] font-medium text-darkBlue transition duration-300 hover:text-vividRed ${
+                                        isProgramsActive ? "text-vividRed" : ""
+                                    } cursor-pointer`}
+                                    onMouseEnter={() =>
+                                        handleMouseHover(menuItem)
+                                    }
+                                    onMouseLeave={() =>
+                                        handleMouseLeave(menuItem)
+                                    }
                                 >
-                                    {menuItem.subMenu?.map((menu) => (
-                                        <NavLink
-                                            key={menu.id}
-                                            to={menu.to}
-                                            className={`px-4 py-2 font-rubik text-[17px] font-medium block whitespace-nowrap ${
-                                                pathname === menu.to
-                                                    ? "bg-lightGreen text-white"
-                                                    : ""
-                                            } hover:bg-lightGreen hover:text-white`}
-                                        >
-                                            {menu.name}
-                                        </NavLink>
-                                    ))}
+                                    {menuItem.name}
+                                    <div
+                                        className={`transform ${
+                                            isProgramsHovered
+                                                ? "translate-x-0"
+                                                : "-translate-x-[100vw]"
+                                        } absolute top-20 left-0 divide-y z-50 shadow-lg divide-[#e5e5e52b] bg-darkBlue text-white transition delay-100 duration-300 ease-in-out`}
+                                    >
+                                        {menuItem.subMenu?.map((menu) => (
+                                            <NavLink
+                                                key={menu.id}
+                                                to={menu.to}
+                                                className={`px-4 py-2 font-rubik text-[17px] font-medium block whitespace-nowrap ${
+                                                    pathname === menu.to
+                                                        ? "bg-lightGreen text-white"
+                                                        : ""
+                                                } hover:bg-lightGreen hover:text-white`}
+                                            >
+                                                {menu.name}
+                                            </NavLink>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                        ) : (
-                            <NavLink
-                                key={menuItem.id}
-                                to={menuItem.to}
-                                className={({ isActive }) =>
-                                    [
-                                        isActive ? "text-vividRed" : "",
-                                        "relative px-default py-7 font-rubik text-[17px] font-medium text-darkBlue transition duration-300 hover:text-vividRed",
-                                    ].join(" ")
-                                }
-                            >
-                                {menuItem.name}
-                            </NavLink>
-                        )
-                    )}
-                </div>
+                            ) : (
+                                <NavLink
+                                    key={menuItem.id}
+                                    to={menuItem.to}
+                                    className={({ isActive }) =>
+                                        [
+                                            isActive ? "text-vividRed" : "",
+                                            "relative px-default py-7 font-rubik text-[17px] font-medium text-darkBlue transition duration-300 hover:text-vividRed",
+                                        ].join(" ")
+                                    }
+                                >
+                                    {menuItem.name}
+                                </NavLink>
+                            )
+                        )}
+                    </div>
 
-                <div className="hidden lg:block">
-                    <LinkButton
-                        name="Contact Us"
-                        icon={<MdOutlineContactPage size="16" />}
-                        to="/contact"
-                        bgColor="red"
-                    />
-                </div>
-
-                <div className="flex items-center gap-6 lg:hidden">
-                    <div className="hidden sm:block">
+                    <div className="hidden lg:block">
                         <LinkButton
                             name="Contact Us"
                             icon={<MdOutlineContactPage size="16" />}
@@ -176,18 +175,29 @@ const Nav = () => {
                         />
                     </div>
 
-                    <div className="">
-                        <SlMenu
-                            onClick={handleMobileMenuClick}
-                            className="text-vividRed"
-                            size="26"
-                        />
-                        {isMobileMenuOpen && (
-                            <MobileMenu
-                                isOpen={isMobileMenuOpen}
-                                setIsOpen={setIsMobileMenuOpen}
+                    <div className="flex items-center gap-6 lg:hidden">
+                        <div className="hidden sm:block">
+                            <LinkButton
+                                name="Contact Us"
+                                icon={<MdOutlineContactPage size="16" />}
+                                to="/contact"
+                                bgColor="red"
                             />
-                        )}
+                        </div>
+
+                        <div className="">
+                            <SlMenu
+                                onClick={handleMobileMenuClick}
+                                className="text-vividRed"
+                                size="26"
+                            />
+                            {isMobileMenuOpen && (
+                                <MobileMenu
+                                    isOpen={isMobileMenuOpen}
+                                    setIsOpen={setIsMobileMenuOpen}
+                                />
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
